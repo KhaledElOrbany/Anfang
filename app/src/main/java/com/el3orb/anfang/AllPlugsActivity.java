610ad86666e8a20f5cc2a6ad62005dae6fa0b97e
Khaled Wagdy <khaled.wagdy554@gmail.com>
2021-12-15 21:45:56 +0300

    - Add features to SinglePlugActivity & XML - Add single_plug_menu.xml - Edit activity_all_plugs.xml

--- app/src/main/java/com/el3orb/anfang/AllPlugsActivity.java
+++ app/src/main/java/com/el3orb/anfang/AllPlugsActivity.java
@@ -5,7 +5,6 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -49,19 +48,17 @@ public class AllPlugsActivity extends AppCompatActivity {
     private void addCard(String name, boolean state) {
         final View view = getLayoutInflater().inflate(R.layout.plug_card, null);
 
-        TextView plugName = view.findViewById(R.id.plugName);
-        Button btnDetails = view.findViewById(R.id.btnOpenPlugDetail);
-        Switch stateSwitch = view.findViewById(R.id.switchPlug);
-
-        plugName.setText(name);
-        stateSwitch.setChecked(state);
-
-        btnDetails.setOnClickListener(v -> {
+        view.setOnClickListener(v -> {
             Intent plugDetails = new Intent(AllPlugsActivity.this, SinglePlugActivity.class);
             plugDetails.putExtra("plugName", name);
             startActivity(plugDetails);
         });
 
+        ((TextView) view.findViewById(R.id.plugName)).setText(name);
+        ((Switch) view.findViewById(R.id.switchPlug)).setChecked(state);
         layout.addView(view);
+    }
+
+    public void addNewPlug(View view) {
     }
 }
--- app/src/main/java/com/el3orb/anfang/SinglePlugActivity.java
+++ app/src/main/java/com/el3orb/anfang/SinglePlugActivity.java
@@ -1,8 +1,13 @@
 package com.el3orb.anfang;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
@@ -26,4 +31,27 @@ public class SinglePlugActivity extends AppCompatActivity {
         setTitle(plugName);
         plug = prayerRef.child("/hm01/plugs" + plugName);
     }
+
+    public void editPlugName(View view) {
+    }
+
+    //region Menu
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.single_plug_menu, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemEditName:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    //endregion
 }
