b965cb5c5983ef8bfa27f16563a2293ee7a448ee
Khaled Wagdy <khaled.wagdy554@gmail.com>
2021-12-21 22:16:29 +0300

    - first implement for charts

--- app/src/main/java/com/el3orb/anfang/SinglePlugActivity.java
+++ app/src/main/java/com/el3orb/anfang/SinglePlugActivity.java
@@ -2,6 +2,7 @@ package com.el3orb.anfang;
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,13 +14,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class SinglePlugActivity extends AppCompatActivity {
     FirebaseDatabase rootRef = FirebaseDatabase.getInstance();
     DatabaseReference prayerRef = rootRef.getReference(), plug;
     String plugId, plugName;
+
+    private LineChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +44,37 @@ public class SinglePlugActivity extends AppCompatActivity {
         this.plugName = extras.getString("plugName");
         setTitle(plugName);
         plug = prayerRef.child("/hm01/plugs/" + plugId + "/");
+        fillLineChart();
+    }
+
+    private void fillLineChart() {
+        mChart = (LineChart) findViewById(R.id.lineChart);
+
+//        mChart.setOnChartGestureListener(MainActivity.this);
+//        mChart.setOnChartValueSelectedListener(MainActivity.this);
+
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(false);
+
+        ArrayList<Entry> yValues = new ArrayList<>();
+        yValues.add(new Entry(0, 60f));
+        yValues.add(new Entry(1, 50f));
+        yValues.add(new Entry(2, 70f));
+        yValues.add(new Entry(3, 90f));
+        yValues.add(new Entry(4, 60f));
+        yValues.add(new Entry(5, 30f));
+        yValues.add(new Entry(6, 80f));
+
+        LineDataSet set1 = new LineDataSet(yValues, "Amper");
+        set1.setFillAlpha(110);
+        set1.setColor(Color.RED);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        LineData data = new LineData(dataSets);
+
+        mChart.setData(data);
     }
 
     public void editPlugName() {
